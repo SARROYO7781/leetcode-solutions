f89f4e9d5ef4f289280a3d84e1031eff0a86d71e
SARROYO7781 <mcedeno1073@icloud.com>
2026-04-24 18:25:02 -0400

    Create 65_Valid_Number.cs

---
+++ Hard/65_Valid_Number.cs
@@ -0,0 +1,8 @@
+using System.Text.RegularExpressions;
+
+public class Solution {
+    public bool IsNumber(string s) {
+        string pattern = @"^[\+\-]?((\d+\.?\d*)|(\.\d+))([eE][\+\-]?\d+)?$";
+        return Regex.IsMatch(s, pattern);
+    }
+}
